cea749fe56c3faa122e39f08a984776f3075c5f5
flatulek <grebneva.sofya.nik@gmail.com>
2024-10-23 15:06:20 +0700

    1 ad 2 task is complite

--- task_3/start.sh
+++ task_3/start.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
 for i in {1..20}; do
-mkdir -p "$i"
-echo "$i.txt" > "$i/$i.txt"
-
-ln -s "$i/$i.txt" "$i.txt"
+  mkdir -p "$i"
+  echo "$i.txt" >> "$i/$i.txt"
+  ln -s "$i/$i.txt" "$i.txt"
 done
+
