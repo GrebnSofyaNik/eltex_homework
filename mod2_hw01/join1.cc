921ad53ed68f6fa55226cc2a4bc5901eefd53f9a
flatulek <grebneva.sofya.nik@gmail.com>
2024-11-26 15:40:19 +0700

    pow 2

---
+++ mod2_hw01/join1.cc
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+#define BASE 2
+
+int main() {
+    int Number;
+    
+    printf("Введите натуральное число N: ");
+    scanf("%d", &Number);
+    
+    if (Number < 1) {
+        printf("Ошибка: число должно быть натуральным.\n");
+        return 1;
+    }
+
+    long long result = (long long)pow(BASE, Number);
+
+    printf("2 в степени %d равно %lld\n", Number, result);
+
+    return 0;
+}
